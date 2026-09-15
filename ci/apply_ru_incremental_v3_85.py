76a6e20a330e8fa4d46e280fdcdd5c446cb00459
QarroSSB <qarrossb@gmail.com>
2026-09-15 15:07:48 +0300

    ci: apply safe Resort Fuchsia Cerulean localization v3.104

--- ci/apply_ru_incremental_v3_85.py
+++ ci/apply_ru_incremental_v3_85.py
@@ -98,6 +98,7 @@ SCRIPTS = [
     "localize_gyms_route25_v3_100.py",
     "localize_saffron_silph_v3_101.py",
     "localize_celadon_mtmoon_route21_v3_102.py",
+    "localize_resort_fuchsia_cerulean_v3_104.py",
 ]
 
 VERSION_RE = re.compile(r"_v3_(\d+)\.py$")
