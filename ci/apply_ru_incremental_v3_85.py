fbbd2c8daeff122b2fc14338e97ddfc6fe11ca89
QarroSSB <qarrossb@gmail.com>
2026-09-16 01:02:56 +0300

    ci: stage Tanoby Ruins Five Island Meadow localization v3.122 manifest

--- ci/apply_ru_incremental_v3_85.py
+++ ci/apply_ru_incremental_v3_85.py
@@ -116,6 +116,7 @@ SCRIPTS = [
     "localize_silph_2f_8f_9f_v3_119.py",
     "localize_silph_3f_4f_10f_v3_120.py",
     "localize_pokemon_tower_3f_4f_6f_7f_v3_121.py",
+    "localize_tanoby_meadow_v3_122.py",
 ]
 
 VERSION_RE = re.compile(r"_v3_(\d+)\.py$")
