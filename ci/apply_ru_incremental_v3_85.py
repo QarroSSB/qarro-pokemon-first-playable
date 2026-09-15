4a852fd6f404a337689ab1b1e60bf75907751ccc
QarroSSB <qarrossb@gmail.com>
2026-09-15 11:43:01 +0300

    ci: include Route 3 9 and 16 localization

--- ci/apply_ru_incremental_v3_85.py
+++ ci/apply_ru_incremental_v3_85.py
@@ -89,6 +89,7 @@ SCRIPTS = [
     "localize_routes13_17_v3_90.py",
     "localize_routes11_12_20_v3_91.py",
     "fix_bulk_localization_escapes_v3_92.py",
+    "localize_routes3_9_16_v3_93.py",
 ]
 
 VERSION_RE = re.compile(r"_v3_(\d+)\.py$")
