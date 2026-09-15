79128531ef84a081561730c90c9cd2bc342c1db0
QarroSSB <qarrossb@gmail.com>
2026-09-15 13:03:34 +0300

    ci: include Saffron and Silph localization

--- ci/apply_ru_incremental_v3_85.py
+++ ci/apply_ru_incremental_v3_85.py
@@ -96,6 +96,7 @@ SCRIPTS = [
     "localize_sevault_canyon_v3_97.py",
     "localize_sevii_dojo_v3_99.py",
     "localize_gyms_route25_v3_100.py",
+    "localize_saffron_silph_v3_101.py",
 ]
 
 VERSION_RE = re.compile(r"_v3_(\d+)\.py$")
