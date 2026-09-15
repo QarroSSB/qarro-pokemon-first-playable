12c3f593a483fef85b78787ad1d5ed0c8d5702be
QarroSSB <qarrossb@gmail.com>
2026-09-15 23:04:34 +0300

    ci: apply Pewter Vermilion Gym localization v3.111

--- ci/apply_ru_incremental_v3_85.py
+++ ci/apply_ru_incremental_v3_85.py
@@ -105,6 +105,7 @@ SCRIPTS = [
     "localize_city_museum_v3_108.py",
     "localize_silph11_ruinvalley_fishing_v3_109.py",
     "localize_rocket_hideout_b1f_v3_110.py",
+    "localize_pewter_vermilion_gym_v3_111.py",
 ]
 
 VERSION_RE = re.compile(r"_v3_(\d+)\.py$")
