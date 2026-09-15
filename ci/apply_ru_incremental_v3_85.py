daba7c71fa66b975fe022b05df0f7c4291070cea
QarroSSB <qarrossb@gmail.com>
2026-09-16 02:39:01 +0300

    ci: stage v3.128 localization manifest on corrected v3.127

--- ci/apply_ru_incremental_v3_85.py
+++ ci/apply_ru_incremental_v3_85.py
@@ -122,6 +122,7 @@ SCRIPTS = [
     "localize_lavender_fishing_v3_125.py",
     "localize_lavender_move_deleter_heracross_v3_126.py",
     "localize_celadon_condominiums_v3_127.py",
+    "localize_museum_lab_chansey_v3_128.py",
 ]
 
 VERSION_RE = re.compile(r"_v3_(\d+)\.py$")
