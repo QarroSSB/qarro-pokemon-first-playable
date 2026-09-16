7947163941071e3fea4df284c07b8084382da763
QarroSSB <qarrossb@gmail.com>
2026-09-17 01:17:21 +0300

    ci: activate full-surface system UI localization v3.151

--- ci/apply_ru_incremental_v3_85.py
+++ ci/apply_ru_incremental_v3_85.py
@@ -139,6 +139,7 @@ SCRIPTS = [
     "localize_full_surface_system_v3_148.py",
     "localize_cable_club_help_v3_149.py",
     "localize_full_surface_system_v3_150.py",
+    "localize_full_surface_system_v3_151.py",
 ]
 
 VERSION_RE = re.compile(r"_v3_(\d+)\.py$")
