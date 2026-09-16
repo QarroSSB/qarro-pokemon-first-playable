c35d68de2de14a5e7234e1db9b12441a9fc201a9
QarroSSB <qarrossb@gmail.com>
2026-09-16 11:47:34 +0300

    ci: activate contract-safe system runtime localization v3.146

--- ci/apply_ru_incremental_v3_85.py
+++ ci/apply_ru_incremental_v3_85.py
@@ -134,6 +134,7 @@ SCRIPTS = [
     "localize_residual_runtime_v3_140_142.py",
     "localize_residual_runtime_v3_143.py",
     "localize_system_runtime_accentfix_v3_145.py",
+    "localize_system_runtime_v3_146.py",
 ]
 
 VERSION_RE = re.compile(r"_v3_(\d+)\.py$")
