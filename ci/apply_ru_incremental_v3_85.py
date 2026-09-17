9d32a79be5846a1fc2e84a91a29855c5c25d03d8
QarroSSB <qarrossb@gmail.com>
2026-09-17 03:31:46 +0300

    ci: activate Move Relearner nurse localization v3.155

--- ci/apply_ru_incremental_v3_85.py
+++ ci/apply_ru_incremental_v3_85.py
@@ -143,6 +143,7 @@ SCRIPTS = [
     "localize_full_surface_system_v3_152.py",
     "localize_full_surface_system_v3_153.py",
     "localize_save_pc_v3_154.py",
+    "localize_move_relearner_nurse_v3_155.py",
 ]
 
 VERSION_RE = re.compile(r"_v3_(\d+)\.py$")
