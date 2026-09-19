f5687c4a48483494bc55b78a8b8bccab38a9e3c2
QarroSSB <qarrossb@gmail.com>
2026-09-20 01:36:54 +0300

    localization: human-polish dynamic strings v3.199

--- ci/apply_ru_incremental_v3_85.py
+++ ci/apply_ru_incremental_v3_85.py
@@ -183,6 +183,7 @@ SCRIPTS = [
     "localize_strings_core_quality_v3_196.py",
     "localize_strings_ui_quality_v3_197.py",
     "localize_strings_easychat_quality_v3_198.py",
+    "localize_strings_dynamic_quality_v3_199.py",
 ]
 
 VERSION_RE = re.compile(r"_v3_(\d+)\.py$")
