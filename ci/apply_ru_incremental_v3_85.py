210fa1b24b40f26455487d7fd893028db13e9789
QarroSSB <qarrossb@gmail.com>
2026-09-20 02:44:55 +0300

    localization: human-polish battle effects v3.203

--- ci/apply_ru_incremental_v3_85.py
+++ ci/apply_ru_incremental_v3_85.py
@@ -187,6 +187,7 @@ SCRIPTS = [
     "localize_battle_message_core_quality_v3_200.py",
     "localize_battle_message_progression_quality_v3_201.py",
     "localize_battle_message_status_quality_v3_202.py",
+    "localize_battle_message_effects_quality_v3_203.py",
 ]
 
 VERSION_RE = re.compile(r"_v3_(\d+)\.py$")
