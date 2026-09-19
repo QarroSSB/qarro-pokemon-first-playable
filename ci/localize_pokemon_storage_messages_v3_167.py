03f5745d56198ef1471975b06823e93196fec78f
QarroSSB <qarrossb@gmail.com>
2026-09-19 04:20:12 +0300

    fix: align v3.167 PSS anchors with normalized source

--- ci/localize_pokemon_storage_messages_v3_167.py
+++ ci/localize_pokemon_storage_messages_v3_167.py
@@ -19,13 +19,13 @@ REL = Path("src/pokemon_storage_system.c")
 PINNED_UPSTREAM = "e8bd1cd7b03fc032ea37e3ecd38b379b5d01a1e7"
 
 TRANSLATIONS = {
-    "MSG_HOLDING_POKE": ("You're holding a POKéMON!", "Ты держишь ПОКЕМОНА!"),
+    "MSG_HOLDING_POKE": ("You're holding a POKeMON!", "Ты держишь ПОКЕМОНА!"),
     "MSG_WHICH_ONE_WILL_TAKE": ("Which one will you take?", "Кого ты возьмёшь?"),
     "MSG_CANT_RELEASE_EGG": ("You can't release an EGG.", "Нельзя отпустить ЯЙЦО."),
     "MSG_CONTINUE_BOX": ("Continue BOX operations?", "Продолжить работу с БОКСОМ?"),
     "MSG_WORRIED": ("Was it worried about you?", "Он переживал за тебя?"),
     "MSG_PLEASE_REMOVE_MAIL": ("Please remove the MAIL.", "Убери ПОЧТУ."),
-    "MSG_GIVE_TO_MON": ("GIVE to a POKéMON?", "ДАТЬ ПОКЕМОНУ?"),
+    "MSG_GIVE_TO_MON": ("GIVE to a POKeMON?", "ДАТЬ ПОКЕМОНУ?"),
     "MSG_BAG_FULL": ("The BAG is full.", "СУМКА заполнена."),
     "MSG_PUT_IN_BAG": ("Put this item in the BAG?", "Положить предмет в СУМКУ?"),
     "MSG_CANT_STORE_MAIL": ("MAIL can't be stored!", "ПОЧТУ нельзя хранить!"),
