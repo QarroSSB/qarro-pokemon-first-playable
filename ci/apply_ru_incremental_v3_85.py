b9d66b223c8d405576cf3f7ebcf05208cab4ed8b
QarroSSB <qarrossb@gmail.com>
2026-09-19 11:39:42 +0300

    localization: human-polish trainers Route 115b-117a v3.184

--- ci/apply_ru_incremental_v3_85.py
+++ ci/apply_ru_incremental_v3_85.py
@@ -170,6 +170,7 @@ SCRIPTS = [
     "localize_trainers_quality_route109b_110_v3_181.py",
     "localize_trainers_quality_route111_113a_v3_182.py",
     "localize_trainers_quality_route113b_115a_v3_183.py",
+    "localize_trainers_quality_route115b_117a_v3_184.py",
 ]
 
 VERSION_RE = re.compile(r"_v3_(\d+)\.py$")
