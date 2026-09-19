8b4f1892578920c51f09a98d10be51b0d88f0f6a
QarroSSB <qarrossb@gmail.com>
2026-09-19 09:57:40 +0300

    test: finish RU tail and classify intentional English v3.178

--- ci/apply_ru_incremental_v3_85.py
+++ ci/apply_ru_incremental_v3_85.py
@@ -164,6 +164,7 @@ SCRIPTS = [
     "localize_shared_dex_all_v3_175.py",
     "localize_credits_roles_v3_176.py",
     "apply_generated_bulk_ru_v3_177.py",
+    "localize_finish_tail_cleanup_v3_178.py",
 ]
 
 VERSION_RE = re.compile(r"_v3_(\d+)\.py$")
