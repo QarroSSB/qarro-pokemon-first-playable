87ee2c34c3b16a24d6b2663ec63f1f2a04632d81
QarroSSB <qarrossb@gmail.com>
2026-09-20 03:30:29 +0300

    localization: human-polish miscellaneous battle effects v3.208

--- ci/apply_ru_incremental_v3_85.py
+++ ci/apply_ru_incremental_v3_85.py
@@ -192,6 +192,7 @@ SCRIPTS = [
     "localize_battle_message_capture_quality_v3_205.py",
     "localize_battle_message_obedience_safari_quality_v3_206.py",
     "localize_battle_message_item_recovery_quality_v3_207.py",
+    "localize_battle_message_misc_effects_quality_v3_208.py",
 ]
 
 VERSION_RE = re.compile(r"_v3_(\d+)\.py$")
